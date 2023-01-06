09cac1b2ded2fbdc729d62f52da25ccc09a049f9
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-01-06 22:17:08 +0000

    Extract engine module

--- src/browser.rs
+++ src/browser.rs
@@ -1,6 +1,6 @@
-use web_sys::{Window, Document, HtmlCanvasElement, CanvasRenderingContext2d, Response};
+use web_sys::{Window, Document, HtmlCanvasElement, CanvasRenderingContext2d, Response, HtmlImageElement};
 use anyhow::{anyhow, Result};
-use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen::{closure::{Closure, WasmClosureFnOnce}, JsCast, JsValue};
 use futures::Future;
 use wasm_bindgen_futures::JsFuture;
 
@@ -61,4 +61,15 @@ pub async fn fetch_json(json_path: &str) -> Result<JsValue> {
     )
     .await
     .map_err(|err| anyhow!("Error fetching JSON {:#?}.", err))
+}
+
+pub fn new_image() -> Result<HtmlImageElement> {
+    HtmlImageElement::new().map_err(|err| anyhow!("Could not create HtmlImageElement: {:#?}.", err))
+}
+
+pub fn closure_once<F, A, R>(fn_once: F) -> Closure<F::FnMut>
+where
+    F: 'static + WasmClosureFnOnce<A, R>,
+{
+    Closure::once(fn_once)
 }
