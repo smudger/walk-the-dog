f6356c888db93caff1bf4c2a8b0d6597dae4fc2a
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-01-07 11:27:44 +0000

    Add Walk The Dog game

--- src/engine.rs
+++ src/engine.rs
@@ -7,6 +7,7 @@ use std::sync::Mutex;
 use std::cell::RefCell;
 use crate::browser;
 use crate::browser::LoopClosure;
+use async_trait::async_trait;
 
 pub async fn load_image(source: &str) -> Result<HtmlImageElement>
 {
@@ -34,9 +35,11 @@ pub async fn load_image(source: &str) -> Result<HtmlImageElement>
     Ok(image)
 }
 
+#[async_trait(?Send)]
 pub trait Game {
+    async fn initialize(&self) -> Result<Box<dyn Game>>;
     fn update(&mut self);
-    fn draw(&self, context: &CanvasRenderingContext2d);
+    fn draw(&self, renderer: &Renderer);
 }
 
 const FRAME_SIZE: f32 = 1.0 / 60.0 * 1000.0;
@@ -48,10 +51,14 @@ pub struct GameLoop {
 type SharedLoopClosure = Rc<RefCell<Option<LoopClosure>>>;
 
 impl GameLoop {
-    pub async fn start(mut game: impl Game + 'static) -> Result<()> {
+    pub async fn start(game: impl Game + 'static) -> Result<()> {
+        let mut game = game.initialize().await?;
         let mut game_loop = GameLoop {
             last_frame: browser::now()?,
             accumulated_delta: 0.0,
+        };
+        let renderer = Renderer {
+            context: browser::context()?,
         };
         let f: SharedLoopClosure = Rc::new(RefCell::new(None));
         let g = f.clone();
@@ -65,7 +72,7 @@ impl GameLoop {
                     game_loop.accumulated_delta -= FRAME_SIZE;
                 }
                 game_loop.last_frame = perf;
-                game.draw(&browser::context().expect("Context should exist."));
+                game.draw(&renderer);
                 browser::request_animation_frame(f.borrow().as_ref().unwrap());
         }));
         browser::request_animation_frame(
@@ -73,5 +80,42 @@ impl GameLoop {
                 .as_ref()
                 .ok_or_else(|| anyhow!("GameLoop: Loop is None."))?)?;
         Ok(())
+    }
+}
+
+pub struct Rect {
+    pub x: f32,
+    pub y: f32,
+    pub width: f32,
+    pub height: f32,
+}
+
+pub struct Renderer {
+    context: CanvasRenderingContext2d,
+}
+
+impl Renderer {
+    pub fn clear(&self, rect: &Rect) {
+        self.context.clear_rect(
+            rect.x.into(),
+            rect.y.into(),
+            rect.width.into(),
+            rect.height.into(),
+        );
+    }
+    pub fn draw_image(&self, image: &HtmlImageElement, frame: &Rect, destination: &Rect) {
+        self.context
+            .draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+                &image,
+                frame.x.into(),
+                frame.y.into(),
+                frame.width.into(),
+                frame.height.into(),
+                destination.x.into(),
+                destination.y.into(),
+                destination.width.into(),
+                destination.height.into(),
+            )
+            .expect("Drawing is throwing exceptions! Unrecoverable error.");
     }
 }
