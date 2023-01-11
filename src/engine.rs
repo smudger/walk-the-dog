7e56ca7edad875a244639547bcb5d47e7254c7c2
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-01-11 18:47:36 +0000

    Remove position duplication from Image

--- src/engine.rs
+++ src/engine.rs
@@ -264,14 +264,13 @@ pub struct Point {
 
 pub struct Image {
     element: HtmlImageElement,
-    position: Point,
     bounding_box: Rect,
 }
 
 impl Image {
     pub fn new(element: HtmlImageElement, position: Point) -> Self {
         let bounding_box = Rect::new(position, element.width() as i16, element.height() as i16);
-        Self { element, position, bounding_box }
+        Self { element, bounding_box }
     }
 
     pub fn bounding_box(&self) -> &Rect {
@@ -279,16 +278,15 @@ impl Image {
     }
 
     pub fn draw(&self, renderer: &Renderer) {
-        renderer.draw_entire_image(&self.element, &self.position);
+        renderer.draw_entire_image(&self.element, &self.bounding_box.position);
     }
 
     pub fn move_horizontally(&mut self, distance: i16) {
-        self.set_x(self.position.x + distance);
+        self.set_x(self.bounding_box.x() + distance);
     }
 
     pub fn set_x(&mut self, x: i16) {
         self.bounding_box.set_x(x);
-        self.position.x = x;
     }
 
     pub fn right(&self) -> i16 {
