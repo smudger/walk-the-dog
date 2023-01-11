11b3e4d16c5984d2fbc1c5e3225b2780365ab7a1
Louis Smith <15144861+smudger@users.noreply.github.com>
2023-01-11 17:55:13 +0000

    Move background rather than RHB

--- src/game.rs
+++ src/game.rs
@@ -23,6 +23,12 @@ pub struct Walk {
     platform: Platform,
 }
 
+impl Walk {
+    fn velocity(&self) -> i16 {
+        -self.boy.walking_speed()
+    }
+}
+
 impl WalkTheDog {
     pub fn new() -> Self {
         WalkTheDog::Loading
@@ -69,6 +75,9 @@ impl Game for WalkTheDog {
                 walk.boy.jump();
             }
             walk.boy.update();
+            walk.platform.position.x += walk.velocity();
+            walk.stone.move_horizontally(walk.velocity());
+            walk.background.move_horizontally(walk.velocity());
             for bounding_box in &walk.platform.bounding_boxes() {
                 if walk
                     .boy
@@ -206,6 +215,10 @@ impl RedHatBoy {
 
     fn velocity_y(&self) -> i16 {
         self.state_machine.context().velocity.y
+    }
+
+    fn walking_speed(&self) -> i16 {
+        self.state_machine.context().velocity.x
     }
 }
 
@@ -395,7 +408,6 @@ mod red_hat_boy_states {
             } else {
                 self.frame = 0;
             }
-            self.position.x += self.velocity.x;
             self.position.y += self.velocity.y;
             if self.position.y > FLOOR {
                 self.position.y = FLOOR;
